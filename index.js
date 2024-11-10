71d78f914af6bbe0f92b731e997417d448bc3ede
Divljo31 <nemanjadivljakovic@protonmail.com>
2024-11-10 12:47:42 +0100

    wallet-creation

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const crypto = require('crypto');
 const { Chain, createPublicClient, http, Hex, parseUnits, maxUint128, stringToBytes, } = require('viem');
-const { privateKeyToAccount } = require('viem/accounts');
 const { ethers } = require("ethers");
 
 
 //Global constants
-const TESTNET_RPC = 'https://rpc-polynomial-network-testnet-x0tryg8u1c.t.conduit.xyz';
 const polyContractAddress = '0x52Fdc981472485232587E334c5Ca27F241CbA9AA';
+const fxUSDContractAddress = '0xE814499181A80B0E4b88FF6af5D12eA2D4d23688';
+ 
+const TESTNET_RPC = 'https://rpc-polynomial-network-testnet-x0tryg8u1c.t.conduit.xyz';
 const provider = new ethers.JsonRpcProvider(TESTNET_RPC);
-const signer = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
-const accountId =  "170141183460469231731687303715884105787";
+let signer; 
+let polyContract;
+let accountId;
 
 // Initialize the HermesClient with the desired endpoint
 const hermes = new HermesClient('https://hermes.pyth.network');
@@ -35,24 +37,10 @@ const polyContractABI = [
   "function getOrder(uint128 accountId) external view returns ((uint128 marketId, uint128 accountId, int128 sizeDelta, uint128 settlementStrategyId, uint256 acceptablePrice, bytes32 trackingCode, address referrer))"
 ];
   
-// Create an instance of the contract
-const polyContract = new ethers.Contract(
-  polyContractAddress,
-  polyContractABI,
-  signer
-);
-
-//Creating fxUSD contract instance
-const fxUSDContractAddress = '0xE814499181A80B0E4b88FF6af5D12eA2D4d23688';
 const fxUSDABI = [
   "function approve(address spender, uint256 amount) public returns (bool)"
 ];
 
-const fxUSDContract = new ethers.Contract(
-  fxUSDContractAddress,
-  fxUSDABI,
-  signer
-);
 
 // Function to create wallet from Telegram ID
 function createWalletFromTelegramID(telegramId) {
@@ -60,20 +48,33 @@ function createWalletFromTelegramID(telegramId) {
   const hash = ethers.keccak256(ethers.toUtf8Bytes(telegramId.toString()));
 
   // Use the hash to create a new wallet
-  const wallet = new ethers.Wallet(hash);
+  signer = new ethers.Wallet(hash, provider);
+
+      // Create an instance of the contract
+  polyContract = new ethers.Contract(
+        polyContractAddress,
+        polyContractABI,
+        signer
+      );
   
-  return {
-      address: wallet.address
-  };
+  
+  return signer.address;
 }
 
 
 // Function to approve spending fxUSD
-async function approveSpending(spenderAddress, amount) {
+async function approveSpending(spenderAddress) {
+    //Creating fxUSD contract instance
+    const fxUSDContract = new ethers.Contract(
+      fxUSDContractAddress,
+      fxUSDABI,
+      signer
+      );
+  
   try {
-    
+
     // Send the approve transaction
-    const tx = await fxUSDContract.approve(spenderAddress, amount);
+    const tx = await fxUSDContract.approve(spenderAddress, ethers.MaxUint256);
     // Wait for the transaction to be confirmed
     const receipt = await tx.wait();
     return receipt;
@@ -89,12 +90,13 @@ async function approveSpending(spenderAddress, amount) {
 //function to create new TradingAccount
 async function createNewAccount() {
   
+
     try {
       // Call the createAccount function
       const tx = await polyContract.createAccount();
   
       // Wait for the transaction to be confirmed
-      const receipt = await tx.wait();
+      const receipt = await tx.wait();      
   
     } catch (error) {
       console.error("Error calling createAccount:", error);
@@ -108,7 +110,7 @@ async function createNewAccount() {
 async function getAccountId(ownerAddress) {
 
     const url = `https://perps-api-testnet.polynomial.finance/accounts?owner=${ownerAddress}&ownershipType=Direct`;
-  
+
     try {
       const response = await axios.get(url);
       const accountId = response.data[0].accountId;
@@ -124,7 +126,7 @@ async function getAvailableMargin(accountId){
   try{
 
     const availableMargin = await polyContract.getAvailableMargin(accountId);
-    console.log(availableMargin);
+    //console.log(availableMargin);
     return availableMargin;
 
   } catch (error){
@@ -164,7 +166,6 @@ async function getMarketMetadata(marketId){
 async function modifyCollateral(amountDelta){
 
   try {
-    
     const tx = await polyContract.modifyCollateral(accountId, 0, amountDelta);
     await tx.wait();
     return `Collateral modified successfully. Transaction hash: ${tx.hash}`;
@@ -244,14 +245,15 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 
 // bot function that says hello 
-bot.onText(/\/hello/, async (msg) => {
+bot.onText(/\/get_started/, async (msg) => {
     const chatId = msg.chat.id;
     const name = msg.from.first_name || "there"; // Get the user's first name
-    const tgID = msg.from.id;
+    const tgId = msg.from.id;
+  
+    const address = createWalletFromTelegramID(tgId);
 
     // Send a greeting message
-    
-    bot.sendMessage(chatId, `Hello, ${name}! How can I help you today?`);
+    bot.sendMessage(chatId, `Hello, ${name}! Tjis is your public address ${address} . Please deposit funds`);
     
 });
 
@@ -260,7 +262,8 @@ bot.onText(/\/create_account/, async (msg) => {
     const chatId = msg.chat.id;
     try {
       const account = await createNewAccount();
-      bot.sendMessage(chatId, `New account created`);
+      accountId = await getAccountId(signer.address);
+      bot.sendMessage(chatId, `New account created with ID: ${accountId}`);
     } catch (error) {
       bot.sendMessage(chatId, `Failed to create account: ${error.message}`);
     }
@@ -300,6 +303,19 @@ bot.onText(/\/get_balance/, async (msg) => {
 });
 
 
+// bot function that approves spending fxUSD balance
+bot.onText(/\/approve/, async (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    const balance = await approveSpending(polyContract);
+    bot.sendMessage(chatId, `Funds approved. TxHash: ${balance.transactionHash}`);
+  } catch (error) {
+    bot.sendMessage(chatId, `failed to approve: ${error.message}`);
+  }
+});
+
+
+
 
 //bot function that calls modifyCollateral function to addCollateral to account
 bot.onText(/\/add_collateral (\d+)/, async (msg, match) => {
@@ -321,8 +337,8 @@ bot.onText(/\/add_collateral (\d+)/, async (msg, match) => {
 //bot function that calls modifyCollateral function to withdrawCollateral to account
 bot.onText(/\/withdraw_collateral (\d+)/, async (msg, match) => {
   const chatId = msg.chat.id;
-  let amount = ethers.parseUnits(match[1], 18); // Converts 1 to 1 * 10^18
-  amount = -amount;
+  let amount = ethers.parseUnits(match[1], 18) * BigInt(-1); // Converts 1 to 1 * 10^18
+  amount = BigInt(amount);
   try {
 
     const tx = await modifyCollateral(amount);
@@ -349,10 +365,9 @@ bot.onText(/\/place_order (\d+) (\d+) (\d+)/, async (msg, match) => {
         const orderType = parseInt(match[1]); // This could be 1 or 2
         const marketId = ethers.parseUnits(match[2], 2); // Converts 2 to 200n
         let sizeDelta = ethers.parseUnits(match[3], 10); // Converts 1 to 1 * 10^18
-        let acceptablePrice = await fetchPriceUpdate() - 10000000000; // current price - 100usd
+        let acceptablePrice = await fetchPriceUpdate(); // current price - 100usd
         if(orderType === 2){
-          sizeDelta = -sizeDelta;
-          acceptablePrice = acceptablePrice + 20000000000;
+          sizeDelta = parseUnits(-sizeDelta, 18);
         }
         
         // Additional parameters for commitmentData
@@ -385,17 +400,3 @@ bot.onText(/\/place_order (\d+) (\d+) (\d+)/, async (msg, match) => {
     }
 });
 
-
-//const wallet = createWalletFromTelegramID(1017458808);
-
- getRequiredMarginForOrder();
- getAvailableMargin(accountId);
-//getOrderFees();
-//const tx = polyContract.modifyCollateral(accountId, 0, -1000000000000000000000n);
-//console.log(`Collateral modified successfully. Transaction hash: ${tx.hash}`);
-
-
-
-// bot.on('message', (msg) => {
-//     console.log('Message received:', msg); // Log received messages to the console
-// });
